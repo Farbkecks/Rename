db4081b662a2a3c5c3bba35fa5396188c21c87b7
Farbkecks <98021172+Farbkecks@users.noreply.github.com>
2022-07-22 12:50:36 +0200

    jump cover

--- rename.py
+++ rename.py
@@ -3,15 +3,19 @@ import os
 ordner_list = os.listdir()
 ordner_list.sort()
 ordner_list.remove("rename.py")
-pretext = "Das Känguru Manifest"
+pretext = "Die Känguru Offenbarung"
 
 cd = 1
 for ordner in ordner_list:
+    if(".jpg" in ordner):
+        continue
     names_list = os.listdir(ordner)
     names_list.sort()
     pos = 1
     for names in names_list:
         if(names == "position.sabp.dat"):
+            continue
+        if(".jpg" in names):
             continue
         print(f"{names} -> {pretext} - CD:{cd} - Pos:{pos}.mp3")
         pos += 1
@@ -22,6 +26,8 @@ input("continue?")
 
 cd = 1
 for ordner in ordner_list:
+    if(".jpg" in ordner):
+        continue
     os.chdir(ordner)
     names_list = os.listdir()
     names_list.sort()
@@ -29,8 +35,11 @@ for ordner in ordner_list:
     for names in names_list:
         if(names == "position.sabp.dat"):
             continue
-        print(names)
+        if(".jpg" in names):
+            continue
         os.rename(names, f"{pretext} - CD:{cd} - Pos:{pos}.mp3")
         pos += 1
     cd += 1
     os.chdir("..")
+ 
+
